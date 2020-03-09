455c67e11347c29177a0fa902be5d66513f4fd3a
Edward Parrish <elp49@drexel.edu>
2020-03-09 19:00:18 -0400

    Implemented Settings and GameOptions classes. Renamed Battle class to BattleSession

--- src/code/GameOptions.java
+++ src/code/GameOptions.java
@@ -1,4 +1,170 @@
 package code;
 
-public class GameOptions {
+import java.io.*;
+
+public class GameOptions implements Serializable {
+
+    String filename = "GameOptions.ser";
+
+    int moveTime;
+    int setupTime;
+    static final int MAX_MOVE_TIME = 99;
+    static final int MIN_MOVE_TIME = 1;
+    static final int MAX_SETUP_TIME = 99;
+    static final int MIN_SETUP_TIME = 1;
+
+    public int getMoveTime() {
+        return moveTime;
+    }
+
+    public void setMoveTime(int moveTime) {
+        if (moveTime >= MIN_MOVE_TIME && moveTime <= MAX_MOVE_TIME && moveTime != this.moveTime) {
+            this.moveTime = moveTime;
+            // Update application's move time.
+            updateApplicationsMoveTime();
+        }
+    }
+
+    public int getSetupTime() {
+        return setupTime;
+    }
+
+    public void setSetupTime(int setupTime) {
+        this.setupTime = setupTime;
+        if (setupTime >= MIN_SETUP_TIME && setupTime <= MAX_SETUP_TIME && setupTime != this.setupTime) {
+            this.setupTime = setupTime;
+            // Update application's setup time.
+            updateApplicationsSetupTime();
+        }
+    }
+
+    public GameOptions() {
+        this.moveTime = 5;
+        this.setupTime = 10;
+    }
+
+    public void incrementMoveTime() {
+        if (this.moveTime < MAX_MOVE_TIME) {
+            this.moveTime += 1;
+            // Update application's move time.
+            updateApplicationsMoveTime();
+        }
+    }
+
+    public void decrementMoveTime() {
+        if (this.moveTime > MIN_MOVE_TIME) {
+            this.moveTime -= 1;
+            // Update application's move time.
+            updateApplicationsMoveTime();
+        }
+    }
+
+    public void incrementSetupTime() {
+        if (this.setupTime < MAX_SETUP_TIME) {
+            this.setupTime += 1;
+            // Update application's setup time.
+            updateApplicationsSetupTime();
+        }
+    }
+
+    public void decrementSetupTime() {
+        if (this.setupTime > MIN_SETUP_TIME) {
+            this.setupTime -= 1;
+            // Update application's setup time.
+            updateApplicationsSetupTime();
+        }
+    }
+
+    public void updateApplicationsMoveTime() {
+        // this.moveTime
+        // ...
+    }
+
+    public void updateApplicationsSetupTime() {
+        // this.setupTime
+        // ...
+    }
+
+    public void updateAllGameOptions() {
+        // ...
+        updateApplicationsMoveTime();
+        updateApplicationsSetupTime();
+        // ...
+    }
+
+    public static GameOptions loadGameOptions() {
+        GameOptions gameOptions = null;
+        try {
+            gameOptions = gameOptions.deserialize();
+        } catch (IOException | ClassNotFoundException e) {
+            // If deserialization failed, then load default game options.
+            e.printStackTrace();
+            gameOptions = new GameOptions();
+            // Serialize default game options for future use.
+            gameOptions.serialize();
+        }
+        // Update the game options.
+        gameOptions.updateAllGameOptions();
+        // Return game options.
+        return gameOptions;
+    }
+
+    public void saveGameOptions() {
+        // Update the game options.
+        this.updateAllGameOptions();
+        // Serialize game options.
+        this.serialize();
+    }
+
+    public void serialize() {
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        try {
+            // Serialize GameOptions object to a stream of bytes.
+            fos = new FileOutputStream(this.filename);
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // Close stream pointers.
+            try {
+                if (fos != null)
+                    fos.close();
+                if (oos != null)
+                    oos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public GameOptions deserialize() throws IOException, ClassNotFoundException {
+        // Check if game options file exists.
+        File file = new File(this.filename);
+        if (!file.exists() || !file.isFile())
+            throw new FileNotFoundException(this.filename);
+        GameOptions gameOptions = null;
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        try {
+            // Deserialize GameOptions object from a stream of bytes.
+            fis = new FileInputStream(this.filename);
+            ois = new ObjectInputStream(fis);
+            gameOptions = (GameOptions) ois.readObject();
+        } finally {
+            // Close stream pointers.
+            try {
+                if (ois != null)
+                    ois.close();
+                if (fis != null)
+                    fis.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        // Return deserialized GameOptions object.
+        return gameOptions;
+    }
+
 }
--- src/code/Settings.java
+++ src/code/Settings.java
@@ -1,4 +1,274 @@
 package code;
 
-public class Settings {
+import java.io.*;
+import java.util.ArrayList;
+
+public class Settings implements Serializable {
+
+    String filename = "Settings.ser";
+
+    public enum Language {ENGLISH}
+
+    Language currentLanguage;
+    boolean soundEffectsOn;
+    ArrayList<String> soundEffectEventIDs;
+    boolean musicOn;
+    String musicFilePath;
+    int volumeLevel;
+    static final int MAX_VOLUME_LEVEL = 100;
+    static final int MIN_VOLUME_LEVEL = 0;
+    String colorTheme;
+    ArrayList<String> colorGraphicIDs;
+
+    public Language getCurrentLanguage() {
+        return currentLanguage;
+    }
+
+    public void setCurrentLanguage(Language currentLanguage) {
+        this.currentLanguage = currentLanguage;
+        // Update system language.
+        // ...
+    }
+
+    public boolean isSoundEffectsOn() {
+        return soundEffectsOn;
+    }
+
+    public void setSoundEffectsOn(boolean soundEffectsOn) {
+        if (this.soundEffectsOn != soundEffectsOn) {
+            this.soundEffectsOn = soundEffectsOn;
+            // Update all system sound effects.
+            this.soundEffectEventIDs.forEach((id) -> updateSystemSoundEffect(id));
+        }
+    }
+
+    public ArrayList<String> getSoundEffectEventIDs() {
+        return soundEffectEventIDs;
+    }
+
+    public void setSoundEffectEventIDs(ArrayList<String> soundEffectEventIDs) {
+        if (!this.soundEffectEventIDs.equals(soundEffectEventIDs)) {
+            this.soundEffectEventIDs = soundEffectEventIDs;
+            // Update all system sound effects.
+            this.soundEffectEventIDs.forEach((id) -> updateSystemSoundEffect(id));
+        }
+    }
+
+    public boolean isMusicOn() {
+        return musicOn;
+    }
+
+    public void setMusicOn(boolean musicOn) {
+        if (this.musicOn != musicOn) {
+            this.musicOn = musicOn;
+            // Update system music.
+            // ...
+        }
+    }
+
+    public String getMusicFilePath() {
+        return musicFilePath;
+    }
+
+    public void setMusicFilePath(String musicFilePath) {
+        if (!this.musicFilePath.equals(musicFilePath)) {
+            this.musicFilePath = musicFilePath;
+            // Update system music file.
+            // ...
+        }
+    }
+
+    public int getVolumeLevel() {
+        return volumeLevel;
+    }
+
+    public void setVolumeLevel(int volumeLevel) {
+        if (volumeLevel >= MIN_VOLUME_LEVEL && volumeLevel <= MAX_VOLUME_LEVEL && volumeLevel != this.volumeLevel) {
+            this.volumeLevel = volumeLevel;
+            // Update system volume level.
+            updateSystemVolumeLevel();
+        }
+    }
+
+    public String getColorTheme() {
+        return colorTheme;
+    }
+
+    public void setColorTheme(String colorTheme) {
+        if (!this.colorTheme.equals(colorTheme)) {
+            this.colorTheme = colorTheme;
+            // Update all system color graphics.
+            this.colorGraphicIDs.forEach((id) -> updateSystemColorGraphic(id));
+        }
+    }
+
+    public ArrayList<String> getColorGraphicIDs() {
+        return colorGraphicIDs;
+    }
+
+    public void setColorGraphicIDs(ArrayList<String> colorGraphicIDs) {
+        if (!this.colorGraphicIDs.equals(colorGraphicIDs)) {
+            this.colorGraphicIDs = colorGraphicIDs;
+            // Update all system color graphics.
+            this.colorGraphicIDs.forEach((id) -> updateSystemColorGraphic(id));
+        }
+    }
+
+    public Settings() {
+        this.currentLanguage = Language.valueOf("ENGLISH");
+        this.soundEffectsOn = true;
+        this.soundEffectEventIDs = new ArrayList<>();
+        this.musicOn = true;
+        this.musicFilePath = "";
+        this.volumeLevel = 50;
+        this.colorTheme = "blue";
+        this.colorGraphicIDs = new ArrayList<>();
+    }
+
+    public void updateSystemLanguage() {
+        // this.currentLanguage
+        // ...
+    }
+
+    public void addSoundEffectID(String id) {
+        if (!this.soundEffectEventIDs.contains(id)) {
+            this.soundEffectEventIDs.add(id);
+            // Update this sound effect.
+            updateSystemSoundEffect(id);
+        }
+    }
+
+    public void updateSystemSoundEffect(String id) {
+        // this.soundEffectsOn
+        // ...
+    }
+
+    public void updateSystemMusic() {
+        // this.musicOn
+        // ...
+    }
+
+    public void updateSystemMusicFile() {
+        // this.musicFilePath
+        // ...
+    }
+
+    public void incrementVolumeLevel() {
+        if (this.volumeLevel < MAX_VOLUME_LEVEL) {
+            this.volumeLevel += 1;
+            // Update system volume level.
+            updateSystemVolumeLevel();
+        }
+    }
+
+    public void decrementVolumeLevel() {
+        if (this.volumeLevel > MIN_VOLUME_LEVEL) {
+            this.volumeLevel -= 1;
+            // Update system volume level.
+            updateSystemVolumeLevel();
+        }
+    }
+
+    public void updateSystemVolumeLevel() {
+        // this.volumeLevel
+        // ...
+    }
+
+    public void addColorGraphicID(String id) {
+        if (!this.colorGraphicIDs.contains(id)) {
+            this.colorGraphicIDs.add(id);
+            // Update this color graphic.
+            updateSystemColorGraphic(id);
+        }
+    }
+
+    public void updateSystemColorGraphic(String id) {
+        // this.colorTheme
+        // ...
+    }
+
+    public void updateAllSystemSettings() {
+        updateSystemLanguage();
+        this.soundEffectEventIDs.forEach((id) -> updateSystemSoundEffect(id));
+        updateSystemMusic();
+        updateSystemMusicFile();
+        updateSystemVolumeLevel();
+        this.colorGraphicIDs.forEach((id) -> updateSystemColorGraphic(id));
+    }
+
+    public static Settings loadSettings() {
+        Settings settings = null;
+        try {
+            settings = settings.deserialize();
+        } catch (IOException | ClassNotFoundException e) {
+            // If deserialization failed, then load default settings.
+            e.printStackTrace();
+            settings = new Settings();
+            // Serialize default settings for future use.
+            settings.serialize();
+        }
+        // Update the application's settings.
+        settings.updateAllSystemSettings();
+        // Return settings.
+        return settings;
+    }
+
+    public void saveSettings() {
+        // Update the application's settings.
+        this.updateAllSystemSettings();
+        // Serialize settings.
+        this.serialize();
+    }
+
+    public void serialize() {
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        try {
+            // Serialize Settings object to a stream of bytes.
+            fos = new FileOutputStream(this.filename);
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // Close stream pointers.
+            try {
+                if (fos != null)
+                    fos.close();
+                if (oos != null)
+                    oos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Settings deserialize() throws IOException, ClassNotFoundException {
+        // Check if settings file exists.
+        File file = new File(this.filename);
+        if (!file.exists() || !file.isFile())
+            throw new FileNotFoundException(this.filename);
+        Settings settings = null;
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        try {
+            // Deserialize Settings object from a stream of bytes.
+            fis = new FileInputStream(this.filename);
+            ois = new ObjectInputStream(fis);
+            settings = (Settings) ois.readObject();
+        } finally {
+            // Close stream pointers.
+            try {
+                if (ois != null)
+                    ois.close();
+                if (fis != null)
+                    fis.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        // Return deserialized Settings object.
+        return settings;
+    }
+
 }
